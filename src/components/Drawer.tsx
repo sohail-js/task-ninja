f5c7ef2fc19a515c7f2d31c4b4f764c9a794d218
Abdul Sohail <sohail@Abduls-MacBook-Pro.local>
2025-02-20 21:06:13 +0530

    style(ml-7): drawer styles

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import React from "react";
+import { HiX } from "react-icons/hi";
 
 type Props = {
   title: string;
@@ -11,13 +12,18 @@ type Props = {
 export default function Drawer({ title, children, isOpen, onClose }: Props) {
   return (
     <div
-      hidden={!isOpen}
-      className={`fixed inset-0 bg-black/80 z-50 transition-opacity`}
+      className={classNames(
+        "fixed inset-0 bg-black/80 z-50 transition-opacity duration-200",
+        {
+          "opacity-100": isOpen,
+          "opacity-0 pointer-events-none": !isOpen,
+        }
+      )}
       onClick={onClose}
     >
       <div
         className={classNames(
-          `fixed inset-y-0 right-0 w-2xl bg-white z-50 shadow-lg transform transition-transform dark:bg-gray-800`,
+          `fixed inset-y-0 right-0 w-2xl bg-white z-50 shadow-lg transform transition-transform duration-300 dark:bg-gray-800`,
           {
             "translate-x-0": isOpen,
             "translate-x-full": !isOpen,
@@ -27,7 +33,9 @@ export default function Drawer({ title, children, isOpen, onClose }: Props) {
       >
         <div className="flex justify-between p-4 border-b">
           <h2>{title}</h2>
-          <button onClick={onClose}>Close</button>
+          <button className="cursor-pointer" onClick={onClose}>
+            <HiX />
+          </button>
         </div>
         <div className="p-4">{children}</div>
       </div>
