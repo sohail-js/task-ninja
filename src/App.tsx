02aa8ccbc67166bf62ea5136d2b11f52ec5fcf89
Abdul Sohail <sohail@Abduls-MacBook-Pro.local>
2025-02-21 02:47:58 +0530

    feat(ml-8): undo/redo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 import Button from "./components/Button";
 import Drawer from "./components/Drawer";
@@ -6,6 +6,8 @@ import { Table } from "./components/Table";
 import { Form } from "./components/Form";
 import { DEFAULT_COLUMNS, LOCAL_STORAGE_KEYS } from "./constants";
 import { HiPlus, HiTrash } from "react-icons/hi2";
+import { LuUndo, LuRedo } from "react-icons/lu";
+
 import { getLocalStorage, setLocalStorage } from "./services/localStorage";
 import ColumnsConfig from "./components/ColumnsConfig";
 import { CustomField } from "./types";
@@ -16,6 +18,11 @@ type RecordItem = {
   title: string;
   priority: string;
   status: string;
+};
+
+type HistoryItem = {
+  data: RecordItem[];
+  columns: CustomField[];
 };
 
 function App() {
@@ -32,6 +39,9 @@ function App() {
   );
   const [editData, setEditData] = useState<RecordItem>();
 
+  const undoStack = useRef<HistoryItem[]>([]);
+  const redoStack = useRef<HistoryItem[]>([]);
+
   useEffect(() => {
     console.log("Data changed");
     setLocalStorage(LOCAL_STORAGE_KEYS.tasks, data);
@@ -47,6 +57,10 @@ function App() {
   };
 
   const handleAddTask = (values: RecordItem) => {
+    undoStack.current.push({
+      data: [...data],
+      columns: [...customColumns],
+    });
     if (editData) {
       setData((prevData) => {
         const index = prevData.findIndex((item) => item.id === editData.id);
@@ -71,18 +85,57 @@ function App() {
     closeDrawer();
   };
 
+  const handleUndo = () => {
+    if (undoStack.current.length > 0) {
+      redoStack.current.push({ data, columns: customColumns });
+      const stackItem = undoStack.current.pop();
+      setData(stackItem?.data!);
+      setCustomColumns(stackItem?.columns!);
+    }
+  };
+
+  const handleRedo = () => {
+    if (redoStack.current.length > 0) {
+      undoStack.current.push({ data, columns: customColumns });
+      const stackItem = redoStack.current.pop();
+      setData(stackItem?.data!);
+      setCustomColumns(stackItem?.columns!);
+    }
+  };
+
+  const handleColumnsChange = (columns: CustomField[]) => {
+    undoStack.current.push({ columns: customColumns, data });
+    setCustomColumns(columns.filter((col) => col.editable));
+  };
+
   return (
     <>
       <div className="flex justify-between items-center p-4 pb-0.5">
         <h1 className="text-2xl font-bold">Task Ninja</h1>
 
         <div className="flex items-center gap-2">
+          <Button
+            mode="secondary"
+            onClick={handleUndo}
+            prefix={<LuUndo />}
+            title="Undo"
+            disabled={undoStack.current.length === 0}
+          >
+            <span className="hidden md:inline">Undo</span>
+          </Button>
+
+          <Button
+            mode="secondary"
+            onClick={handleRedo}
+            prefix={<LuRedo />}
+            title="Redo"
+            disabled={redoStack.current.length === 0}
+          >
+            <span className="hidden md:inline">Redo</span>
+          </Button>
           <ColumnsConfig
             columns={[...DEFAULT_COLUMNS, ...customColumns]}
-            onColumnsChange={(columns) => {
-              // saving only custom columns
-              setCustomColumns(columns.filter((col) => col.editable));
-            }}
+            onColumnsChange={handleColumnsChange}
           />
 
           <Button
@@ -126,6 +179,7 @@ function App() {
               label: "Delete",
               onClick: (record) => {
                 if (confirm("Are you sure you want to delete this record?")) {
+                  undoStack.current.push({ data, columns: customColumns });
                   setData((prevData) =>
                     prevData.filter((item) => item.id !== record.id)
                   );
